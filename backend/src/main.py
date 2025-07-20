aa65c79ece1087143d6de5928fa1b90313384539
irinaqqq <madiyaryep@gmail.com>
2025-07-21 00:36:30 +0500

    f

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.core.dependencies import get_config
@@ -6,12 +7,17 @@ from src.routers.all import router as all_routes
 from src.core.logger import logger, setup_logging, logging
 
 setup_logging()
-logger.info(f"✅ Logging initialized. Effective level: {logging.getLevelName(logger.getEffectiveLevel())}")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info(f"✅ Logging initialized. Effective level: {logging.getLevelName(logger.getEffectiveLevel())}")
+    yield 
 
 app = FastAPI(
     title="Chess Video Platform",
     version="1.0.0",
     debug=get_config().DEBUG,
+    lifespan=lifespan,
 )
 
 app.add_middleware(
@@ -22,3 +28,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(all_routes)
+
+
